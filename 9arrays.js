9b2cc1bc9f89d673b63ea5cfd5c7186c94d0863c
Yasmin <yasmin.porto@aluno.senai.br]>
2025-04-25 11:44:03 -0300

    9arrays.js

---
+++ 9arrays.js
@@ -0,0 +1,10 @@
+let frutas = ['Melancia', 'Melao', 'Mamamao', 'Manga', 'Morango', 'Mertilo']
+let total = frutas.length;
+console.log('Frutas')
+console.log(`O total das frutas são : ${total}`);
+console.log('----------------------');
+
+for (let i = 0; i < total; i++){
+
+    console.log(frutas[i])
+}
